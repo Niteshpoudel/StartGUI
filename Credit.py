0696e224d76811fab0fe58cc3437964235b3b622
Niteshpoudel <mast.poudel@gmail.com>
2021-09-06 16:31:59 +0545

    Initial commit

---
+++ Credit.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+def open_credit():
+    root = Toplevel()
+    root.title("Credits")
+    root.iconbitmap("sponge.ico")
+    root.geometry("800x600")
+    root.resizable(width=False, height=False)
+    bg = PhotoImage(file="island2.png")
+
+    # Creating Canvas
+    my_canvas = Canvas(root, width=800, height=600, highlightthickness=0)
+    my_canvas.pack(fill="both", expand=False)
+
+    #Setting Canvas Image
+    my_canvas.create_image(0,0, image=bg, anchor="nw")
+
+    #Creating Label In Canvas
+    my_canvas.create_text(425, 200, text="""
+    All The Credit For Our Billion Dollar Game 
+        Goes To Our Core Team Members. 
+    """, font=("arial",25,'bold'), fill="black")
+
+    my_canvas.create_text(425, 350, text=""" 
+    Subifa Maharjan aka Leader
+    Nishant Raj Dahal aka Rider 
+    Sanjiv Shrestha aka Topper 
+    Nitesh Poudel aka Rinkiya k papa
+    """, font=("arial",23,'bold'), fill="black",)
+
+    mainloop()
---
+++ Setting.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from control import open_control
+import sys
+def open_setting():
+    root = Toplevel()
+    root.title("Setting")
+    root.iconbitmap("sponge.ico")
+    root.geometry("800x600")
+    root.resizable(width=False, height=False)
+    bg = PhotoImage(file="island3.png")
+
+    # Creating Canvas
+    my_canvas = Canvas(root, width=800, height=600, highlightthickness=0)
+    my_canvas.pack(fill="both", expand=False)
+
+    #Setting Canvas Image
+    my_canvas.create_image(0,0, image=bg, anchor="nw")
+    #n, ne, e, se, s, sw, w, nw, or center
+
+    def exit_setting():
+        root.destroy()
+
+    #Creating Control Button
+    control_button = Button(root, text="CONTROL", bg="#59c2dc", bd=0, fg="white", font=("arial", 15, 'bold'), width=15, height=2, command=open_control)
+    control_window = my_canvas.create_window(310, 150, anchor="nw", window=control_button)
+
+    #Creating Sound Button
+    sound_button = Button(root, text="SOUND", bg="#59c2dc", bd=0, fg="white", font=("arial", 15, 'bold'), width=15, height=2)
+    sound_window = my_canvas.create_window(310, 250, anchor="nw", window=sound_button)
+
+    #Creating Highscore Button
+    highscore_button = Button(root, text="HIGHSCORE", bg="#59c2dc",bd=0, fg="white", font=("arial", 15, 'bold'), width=15, height=2)
+    highscore_window = my_canvas.create_window(310, 350, anchor="nw", window=highscore_button)
+
+    #Creating Back Button
+    back_button = Button(root, text="BACK", bg="#59c2dc",bd=0, fg="white", font=("arial", 15, 'bold'), width=15, height=2, command=exit_setting)
+    back_window = my_canvas.create_window(310, 450, anchor="nw", window=back_button)
+
+    mainloop()
